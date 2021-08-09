ec4bc1a9e99babe2c2455065f80764712d6d88f1
Kane F <kanef9x@protonmail.com>
2021-08-09 19:01:55 +0100

    More fixes

--- strongDMI/save.py
+++ strongDMI/save.py
@@ -26,7 +26,7 @@ def saveDMI(self, file_path):
         elif state_node.type() == "grouplayer": # If node is group layer
             for dir_node in state_node.childNodes():
                 if dir_node.type() == "paintlayer":
-                    state_node.save("tmp.png", 72., 72., krita.InfoObject(), dir_node.bounds())
+                    dir_node.save("tmp.png", 72., 72., krita.InfoObject(), dir_node.bounds())
                     current_icon = Image.open("tmp.png")
                     dmi.states[state_node.name()].frames = len(state_node.childNodes()) # Frames is amount of layers under this group
                     dmi.states[state_node.name()].dirs = 1
@@ -34,12 +34,17 @@ def saveDMI(self, file_path):
                     dmi.states[state_node.name()].delay += dir_node.name() # Layer names get converted to frame delay, much better system needed eventually
                     os.remove("tmp.png")
                 elif dir_node.type() == "grouplayer":
+                    dmi.states[state_node.name()].frames = len(dir_node.childNodes())
+                    if len(state_node.childNodes()) >= 8:
+                        dmi.states[state_node.name()].dirs = 8 # Directions are amount of layers under this group, truncated to 8, 4 or 1
+                    elif len(state_node.childNodes()) >= 4:
+                        dmi.states[state_node.name()].dirs = 4
+                    else:
+                        dmi.states[state_node.name()].dirs = 1
                     for frame_node in dir_node.childNodes():
                         if frame_node.type() == "paintlayer":
-                            state_node.save("tmp.png", 72., 72., krita.InfoObject(), frame_node.bounds())
+                            frame_node.save("tmp.png", 72., 72., krita.InfoObject(), frame_node.bounds())
                             current_icon = Image.open("tmp.png")
-                            dmi.states[state_node.name()].frames = len(dir_node.childNodes())
-                            dmi.states[state_node.name()].dirs = 8 if len(state_node.childNodes()) >= 8 else 4 # Directions are amount of layers under this group, truncated to 8 or 4
                             dmi.states[state_node.name()].icons += [current_icon]
                             dmi.states[state_node.name()].delay += frame_node.name()
                             os.remove("tmp.png")
