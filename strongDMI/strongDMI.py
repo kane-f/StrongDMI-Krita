5ecf40d00b0f7c7f46c024a6669227d0d09c2ebb
Kane F <kanef9x@protonmail.com>
2021-08-09 14:48:10 +0100

    Beginning actual extension work

--- strongDMI/strongDMI.py
+++ strongDMI/strongDMI.py
@@ -1,6 +1,8 @@
 from krita import *
 
 class StrongDMI(Extension):
+    from .load import loadDMI
+    from .save import saveDMI
 
     def __init__(self, parent):
         super().__init__(parent)
@@ -11,4 +13,7 @@ class StrongDMI(Extension):
 
     # called after setup(self)
     def createActions(self, window):
-        pass
+        action1 = window.createAction("load_dmi", "Load DMI file", "tools/scripts")
+        #action1.triggered.connect(self.loadDMI("/"))
+        action2 = window.createAction("save_dmi", "Save DMI file", "tools/scripts")
+        #action2.triggered.connect(self.saveDMI("/"))
