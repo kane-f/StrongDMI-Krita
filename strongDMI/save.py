5c5b19c63effc7496530465bf94f114f633daa40
Kane F <kanef9x@protonmail.com>
2021-08-09 01:57:37 +0100

    Saving now works, finally

--- strongDMI/save.py
+++ strongDMI/save.py
@@ -15,29 +15,25 @@ def saveDMI(file_path):
     for state_node in root.childNodes():
         dmi.states[state_node.name()] = State(state_node.name())
         if state_node.type() == "paintlayer":
-            current_icon = Image.open(io.BytesIO(state_node.pixelData(0,0,doc.width(),doc.height())).getvalue())
+            current_icon = Image.frombytes("RGBA",(doc.width(),doc.height()),state_node.pixelData(0,0,doc.width(),doc.height()))
             dmi.states[state_node.name()].dirs = 1
-            dmi.states[state_node.name()].icons[0] = current_icon
-            dmi.states[state_node.name()].delay[0] = 0
+            dmi.states[state_node.name()].icons += [current_icon]
         elif state_node.type() == "grouplayer":
-            i = 0
             for dir_node in state_node.childNodes():
                 if dir_node.type() == "paintlayer":
-                    current_icon = Image.open(io.BytesIO(state_node.pixelData(0,0,doc.width(),doc.height())).getvalue())
+                    current_icon = Image.frombytes("RGBA",(doc.width(),doc.height()),state_node.pixelData(0,0,doc.width(),doc.height()))
                     dmi.states[state_node.name()].frames = len(state_node.childNodes())
                     dmi.states[state_node.name()].dirs = 1
-                    dmi.states[state_node.name()].icons[i] = current_icon
-                    dmi.states[state_node.name()].delay[i] = dir_node.name()
-                    i += 1
+                    dmi.states[state_node.name()].icons += [current_icon]
+                    dmi.states[state_node.name()].delay += dir_node.name()
                 elif dir_node.type() == "grouplayer":
                     for frame_node in dir_node.childNodes():
                         if frame_node.type() == "paintlayer":
-                            current_icon = Image.open(io.BytesIO(state_node.pixelData(0,0,doc.width(),doc.height())).getvalue())
+                            current_icon = Image.frombytes("RGBA",(doc.width(),doc.height()),state_node.pixelData(0,0,doc.width(),doc.height()))
                             dmi.states[state_node.name()].frames = len(dir_node.childNodes())
                             dmi.states[state_node.name()].dirs = len(state_node.childNodes())
-                            dmi.states[state_node.name()].icons[i] = current_icon
-                            dmi.states[state_node.name()].delay[i] = frame_node.name()
-                            i += 1
+                            dmi.states[state_node.name()].icons += [current_icon]
+                            dmi.states[state_node.name()].delay += frame_node.name()
                             
     dmi.icon_width = doc.width()
     dmi.icon_height = doc.height()
