47d566c62c7fd147fd1471a16a6edc2e4e2eb4e3
Kane F <kanef9x@protonmail.com>
2021-08-09 01:16:18 +0100

    Better format

--- strongDMI/save.py
+++ strongDMI/save.py
@@ -11,29 +11,27 @@ def saveDMI(file_path):
     doc = Krita.instance().openDocument(file_path)
     root = doc.rootNode()
     
-    i = 0
     for state_node in root.childNodes():
+        dmi.states[state_node.name()] = State(state_node.name())
         if state_node.type() == "paintlayer":
             current_icon = Image.open(io.BytesIO(state_node.pixelData(0,0,doc.width(),doc.height())))
-            dmi.states[state_node.name()] = State(state_node.name())
-            dmi.states[state_node.name()].frames = 0
             dmi.states[state_node.name()].dirs = 1
-            dmi.states[state_node.name()].icons[i] = current_icon
-            i += 1
+            dmi.states[state_node.name()].icons[0] = current_icon
+            dmi.states[state_node.name()].delay[0] = 0
+        i += 0
         elif state_node.type() == "grouplayer":
             for dir_node in state_node.childNodes():
                 if dir_node.type() == "paintlayer":
                     current_icon = Image.open(io.BytesIO(state_node.pixelData(0,0,doc.width(),doc.height())))
-                    dmi.states[state_node.name()] = State(state_node.name())
                     dmi.states[state_node.name()].frames = len(state_node.childNodes())
                     dmi.states[state_node.name()].dirs = 1
                     dmi.states[state_node.name()].icons[i] = current_icon
+                    dmi.states[state_node.name()].delay[i] = dir_node.name()
                     i += 1
                 elif dir_node.type() == "grouplayer":
                     for frame_node in dir_node.childNodes():
                         if frame_node.type() == "paintlayer":
                             current_icon = Image.open(io.BytesIO(state_node.pixelData(0,0,doc.width(),doc.height())))
-                            dmi.states[state_node.name()] = State(state_node.name())
                             dmi.states[state_node.name()].frames = len(dir_node.childNodes())
                             dmi.states[state_node.name()].dirs = len(state_node.childNodes())
                             dmi.states[state_node.name()].icons[i] = current_icon
